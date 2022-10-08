163d4918f2e73c837afa4251e32b5bb616e43795
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-10-08 01:11:46 -0500

    chore:[1] dependencias y pom para todos los microservicios

---
+++ client-authorization/src/main/java/egardengo/microservicios/clientauthorization/ClientAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package egardengo.microservicios.clientauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAuthorizationApplication.class, args);
+	}
+
+}
---
+++ client-authorization/src/test/java/egardengo/microservicios/clientauthorization/ClientAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package egardengo.microservicios.clientauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
